e22b53321ee8fecfa44e90480325f2b3d6dbd40d
tamtd4 <45614382+tamtd4@users.noreply.github.com>
2019-03-20 09:15:48 +0700

    Update deploy_zabbix.sh

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -5,4 +5,5 @@ SCRIPT_DIR=/usr/local/bin/zabbix
 mkdir -p ${SCRIPT_DIR}/scripts/zapgix
 cp -r ${SOURCE_DIR}/PostgreSQL/sql ${SCRIPT_DIR}/scripts/zapgix/
 cp ${SOURCE_DIR}/PostgreSQL/zapgix.sh ${SCRIPT_DIR}/scripts/zapgix/
+sudo chmod +x ${SCRIPT_DIR}/scripts/zapgix/zapgix.sh
 cp ${SOURCE_DIR}/PostgreSQL/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/userparameter_postgresql.conf
